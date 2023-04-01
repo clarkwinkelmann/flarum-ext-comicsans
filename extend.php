9eb3a2859b8b4372b42adc9042572f5b7f4c3e6b
Clark Winkelmann <clark.winkelmann@gmail.com>
2023-04-01 15:17:17 +0200

    Add table formatting

--- extend.php
+++ extend.php
@@ -4,6 +4,11 @@ namespace ClarkWinkelmann\ComicSans;
 
 use Flarum\Extend;
 use s9e\TextFormatter\Configurator;
+use s9e\TextFormatter\Utils;
+
+const MAX_OFFSET = 15;
+const MIN_OFFSET = 0;
+const MAX_RANDOM_OFFSET_CHANGE = 5;
 
 return [
     (new Extend\Frontend('forum'))
@@ -14,5 +19,40 @@ return [
                 '[COMIC]{TEXT}[/COMIC]',
                 '<span class="comicsans">{TEXT}</span>'
             );
+
+            $config->BBcodes->addCustom(
+                '[COMIC-TABLE]{ANYTHING}[/COMIC-TABLE]',
+                '<table class="comicsans-table">{ANYTHING}</table>'
+            );
+
+            $config->BBCodes->addFromRepository('THEAD');
+            $config->BBCodes->addFromRepository('TBODY');
+            $config->BBCodes->addFromRepository('TR');
+            $config->BBCodes->addFromRepository('TH');
+            $config->BBCodes->addFromRepository('TD');
+
+            $tdTemplate = $config->tags['TD']->template->asDOM();
+
+            foreach ($tdTemplate->getElementsByTagName('td') as $node) {
+                $node->insertAdjacentXML(
+                    'afterbegin',
+                    '<xsl:if test="@offset">
+                        <xsl:attribute name="style">--offset: <xsl:value-of select="@offset"/></xsl:attribute>
+                    </xsl:if>'
+                );
+            }
+
+            $tdTemplate->saveChanges();
         })
+        ->render(function ($renderer, $context, $xml) {
+            $offset = 0;
+
+            return Utils::replaceAttributes($xml, 'TD', function ($attributes) use (&$offset) {
+                $offset = max(min($offset + random_int(-min($offset - MIN_OFFSET, MAX_RANDOM_OFFSET_CHANGE), min(MAX_OFFSET - $offset, MAX_RANDOM_OFFSET_CHANGE)), MAX_OFFSET), MIN_OFFSET);
+
+                return $attributes + [
+                        'offset' => $offset,
+                    ];
+            });
+        }),
 ];
